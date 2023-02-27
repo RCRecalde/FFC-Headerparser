e804a454acaa105f1df623fb672a1430f392dcc9
RCRecalde <rorecalde3@gmail.com>
2023-02-27 15:18:12 -0300

    ffc project

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Request Header Parser Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice)
+
+- GET REQUEST /api/whoami: devuelve JSON con IP, lenguaje y software.
---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let object = {}
+app.enable('trust proxy') //muestra la ip de la computadora del usuario en formato conocido
+app.get('/api/whoami', (req, res) => {
+  object['ipaddress'] = req.ip 
+  object['language'] = req.get('Accept-Language')//el nombre del header devuelve su contenido
+  object['software'] = req.get('User-Agent') //header para la informacion del software
+  res.json(object)
+})
